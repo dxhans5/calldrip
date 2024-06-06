dcbb56241f5e1a0a465e8579d6b2538a089f4281
unknown <dxhans5@gmail.com>
2024-06-06 03:22:34 -0600

    fixed title of page

--- resources/views/crypto/index.blade.php
+++ resources/views/crypto/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Virus's Lyric Database</title>
+    <title>Virus's Crypto Index</title>
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
